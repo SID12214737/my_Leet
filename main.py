24eedf1d185df568cedeb49222067ea60bf3eb93
sid12214737 <uzmir6358@gmail.com>
2025-01-14 21:16:04 +0500

    2657

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ class Solution(object):
         :type B: List[int]
         :rtype: List[int]
         """
+        frequency_array = {}
         
+        out = [1] if A[0] == B[0] else [0]
+        
+        for i in range(1, len(A)):
+            print(A[i], B[:i+1])
+            if A[i] in B[:i+1]:
+                out.append(out[i-1]+1)
+            else: 
+                out.append(out[i-1])
+        return out
 
 test = [1, 3, 2, 4], [3, 1, 2, 4]
 print(Solution().findThePrefixCommonArray(*test))
